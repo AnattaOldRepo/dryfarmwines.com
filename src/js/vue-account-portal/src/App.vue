ff2dd9e95e71953f6ac333315cb7b233487ebe5d
Alex Avila <alex.avilx@icloud.com>
2019-01-24 18:45:13 -0800

    Test eslintignore 3

--- src/js/vue-account-portal/src/App.vue
+++ src/js/vue-account-portal/src/App.vue
@@ -21,7 +21,7 @@ import DrawerOverlay from './components/drawer/DrawerOverlay.vue'
 import Modal from './components/modal/Modal.vue'
 import ModalOverlay from './components/modal/ModalOverlay.vue'
 import EmptyPage from './pages/EmptyPage.vue'
-import { loadRechargeData } from './assets/js'
+import { loadRechargeData } from './assets/js';
 import { mapGetters, mapMutations } from 'vuex';
 export default {
   components: {
